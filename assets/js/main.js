58d56ba9cd6116a44ac68ac67869f02f18d6bd5a
HeberRios <68972507+HeberRios@users.noreply.github.com>
2024-08-07 13:31:42 -0600

    Get report data from the data.json
    The report stats are now obtained from the data.json file, so when a
    radio button is checked the corresponding timeframe values will be
    displayed.

---
+++ assets/js/main.js
@@ -0,0 +1,99 @@
+// SELECTING ELEMENTS
+
+const timeFrameOptions = document.querySelectorAll("[name='timeframe']");
+const defaultOptionLabel = document.querySelector(".checked");
+const currentTimeframeHours = document.querySelectorAll(".timeframe-hours");
+const previousTimeFrameHours = document.querySelectorAll(
+    ".previous-timeframe-hours"
+);
+
+// FUNCTIONS
+
+function setTimeframeData(timeframeDataArray, timeframeOptionValue) {
+    if (timeframeOptionValue === "daily") {
+        timeframeDataArray.forEach(function (activityData, index) {
+            currentTimeframeHours[
+                index
+            ].textContent = `${activityData.current}hrs`;
+            previousTimeFrameHours[
+                index
+            ].textContent = `Yesterday - ${activityData.previous}hrs`;
+        });
+    } else if (timeframeOptionValue === "weekly") {
+        timeframeDataArray.forEach(function (activityData, index) {
+            currentTimeframeHours[
+                index
+            ].textContent = `${activityData.current}hrs`;
+            previousTimeFrameHours[
+                index
+            ].textContent = `Last Week - ${activityData.previous}hrs`;
+        });
+    } else {
+        timeframeDataArray.forEach(function (activityData, index) {
+            currentTimeframeHours[
+                index
+            ].textContent = `${activityData.current}hrs`;
+            previousTimeFrameHours[
+                index
+            ].textContent = `Last Month - ${activityData.previous}hrs`;
+        });
+    }
+}
+
+async function getDailyData(timeframeOption) {
+    const response = await fetch("../../assets/json/data.json");
+    const data = await response.json();
+
+    const dataArray = new Array();
+
+    data.forEach(function (activity) {
+        dataArray.push(activity["timeframes"]["daily"]);
+    });
+
+    setTimeframeData(dataArray, timeframeOption);
+}
+
+async function getWeeklyData(timeframeOption) {
+    const response = await fetch("../../assets/json/data.json");
+    const data = await response.json();
+
+    const dataArray = new Array();
+
+    data.forEach(function (activity) {
+        dataArray.push(activity["timeframes"]["weekly"]);
+    });
+
+    setTimeframeData(dataArray, timeframeOption);
+}
+
+async function getMonthlyData(timeframeOption) {
+    const response = await fetch("../../assets/json/data.json");
+    const data = await response.json();
+
+    const dataArray = new Array();
+
+    data.forEach(function (activity) {
+        dataArray.push(activity["timeframes"]["monthly"]);
+    });
+
+    setTimeframeData(dataArray, timeframeOption);
+}
+
+// ADD EVENT LISTENER TO THE TIMEFRAME OPTIONS
+
+timeFrameOptions.forEach(function (option) {
+    option.addEventListener("click", function (e) {
+        defaultOptionLabel.classList.remove("checked");
+        if (e.target.value === "daily") {
+            getDailyData(e.target.value);
+        } else if (e.target.value === "weekly") {
+            getWeeklyData(e.target.value);
+        } else {
+            getMonthlyData();
+        }
+    });
+});
+
+// SHOW WEEKLY DATA AT THE BEGINNING
+
+getWeeklyData("weekly");
